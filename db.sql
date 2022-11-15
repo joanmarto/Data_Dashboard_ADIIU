deef8fa59c23b2320c26eb2e837fc3440c572274
Joan Martorell <joanmartorell23@gmail.com>
2022-11-15 11:25:47 +0100

    Some selects

--- db.sql
+++ db.sql
@@ -1,3 +1,5 @@
+/*DataBase creation*/
+
 CREATE TABLE Municipio(
 	provincia	varchar(25) NOT NULL,
     cod_mun		int,
@@ -25,3 +27,18 @@ CREATE TABLE Voto(
     PRIMARY KEY (cod_mesa, partido),
     FOREIGN KEY (cod_mesa) REFERENCES Mesa(cod_mesa)
 );
+
+/*SELECTS*/
+
+/*Shows each party with they number of votes, order by numer of votes*/
+SELECT partido, SUM(votos) AS Total_Votos FROM voto 
+GROUP BY partido 
+ORDER BY Total_Votos DESC;
+
+/*Shows first and second advance for a given province*/
+/*REVISAR*/
+SELECT partido, SUM(p_av) AS Primer_Avance, SUM(s_av) AS Segundo_Avance FROM municipio JOIN
+mesa ON provincia='LEON' AND municipio.cod_mun=mesa.cod_mun JOIN
+voto ON mesa.cod_mesa=voto.cod_mesa
+GROUP BY partido
+ORDER BY Segundo_Avance DESC
